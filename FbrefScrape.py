9fdbc76393cfe22b19bae633dc7de6bb6dd7ea58
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2023-12-13 21:12:37 -0700

    changed code to only scrape non empty values and commented header code

--- FbrefScrape.py
+++ FbrefScrape.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-url = "https://fbref.com/en/players/d70ce98e/Lionel-Messi"
+url = "https://fbref.com/en/players/774cf58b/Max-Aarons"
 
 r = requests.get(url)
 
@@ -10,29 +10,33 @@ soup = BeautifulSoup(r.text, "lxml")
 
 tables = soup.findAll("table")
 
-fileName = "testReq7.txt"
+summary_entries = []
+label_data = []
+
+# fileName = "testReq7.txt"
 for table in tables:
     footer = table.find("tfoot")
     if footer:
         # print("1")
-        header = table.find_all("tr")
-        labels = header[1]
-        label_data = []
-        for i in labels:
-            info = i.text
-            label_data.append(info)
-        filtered_list = [x for x in label_data if x.strip()]
-        print(filtered_list)
+        # header = table.find_all("tr")
+        # labels = header[1]
+        
+        # for i in labels:
+        #     info = i.text
+        #     label_data.append(info)
+        # filtered_list = [x for x in label_data if x.strip()]
+        
 
         summary = footer.find_all("tr")
         summary_data = summary[0]
-        summary_entries = []
+        
         for i in summary_data:
             data = i.text
-            summary_entries.append(data)
-        print(summary_entries)
-
-
+            if data:
+                summary_entries.append(data)
+        
+# print(filtered_list)
+print(summary_entries)
 
 
 
